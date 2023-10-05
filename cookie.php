e082ebe03c83118f9fb4001e1fdfb16afb57f690
karo2345 <146316995+karo2345@users.noreply.github.com>
2023-10-05 08:21:16 +0200

    Add files via upload

---
+++ cookie.php
@@ -0,0 +1,15 @@
+
+<?php
+    $cookie_name = "user";
+    if(!isset($. COOKIE[$cookie name])) {
+        echo "Cookie named '" . $cookie_name . "' is not set!";
+   
+    } else {
+        echo "Cookie '" .$cookie_name . "' is set! <br>";
+        echo "Value is: ".$ COOKIE[$cookie name];
+    
+ 
+    
+    
+    }
+?>
