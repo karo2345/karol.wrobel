e88076056108d9b659ff7cf01a50698641619ec8
karo2345 <146316995+karo2345@users.noreply.github.com>
2023-10-10 14:32:44 +0200

    Add files via upload

---
+++ Wyszukiwanie pliku z roszczerzeniemgit.php
@@ -0,0 +1,9 @@
+<?php
+$wzorzec = '/\S*\.gif/i';
+
+
+$tekst = "w pliku obrazek.gif jest ikona";
+if( preg_match( $wzorzec, $tekst ) )
+    echo "znaleziono obrazek";
+else
+    echo "nie znaleziono obrazku";
