2e767ab2b5c35aef7893095a081a76ad915db6c2
karo2345 <146316995+karo2345@users.noreply.github.com>
2023-11-02 09:39:49 +0100

    Add files via upload

---
+++ formularz.php
@@ -0,0 +1,10 @@
+<form method="post" action="<?php echo
+htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+Name: <input type="text" name="name" value="<?php echo $name;?>">
+<span class="error">* <?php echo $nameErr;?></span>
+<br><br>
+E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+<span class="error">* <?php echo $emailErr;?></span>
+<br><br>
+Website: <input type="text" name="website" value="<?php echo $website;?>">
+<span class="error"><?php echo $websiteErr;?></span>
